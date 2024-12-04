bc184e23d9df69a9762af0f9020d184dea8c9b2e
davidbetteridge <david.betteridge@zeromission.io>
2024-12-04 09:47:28 +0000

    Day3 faster

--- AdventOfCode2024/AdventOfCode2024.Solutions/Day03/Day03.cs
+++ AdventOfCode2024/AdventOfCode2024.Solutions/Day03/Day03.cs
@@ -30,134 +30,6 @@ public class Day03
         
         //   m  u  l  (  [0  1  2  3  4  5  6  7  8  9]  ,  0  1  2  3  4  5  6  7  8  9 )
         //0 10 11 12 13   14 14 14 14 14 14 14 14 14 14 15 16 16 16 16 16 16 16 16 16 16 17
-
-        var fns = new Func<char, int>[]
-        {
-            //0
-            (c) => c switch {
-                'd' => 1,
-                'm' => 10,
-                _ => 0
-            },
-            
-            //1
-            (c) => c switch {
-                'd' => 1,
-                'm' => 10,
-                'o' => 2,
-                _ => 0
-            },
-            
-            //2
-            (c) => c switch {
-                'd' => 1,
-                'm' => 10,
-                'n' => 3,
-                '(' => 8,
-                _ => 0
-            },
-            
-            //3
-            (c) => c switch {
-                'd' => 1,
-                'm' => 10,
-                '\'' => 4,
-                _ => 0
-            },
-            
-            (c) => c switch {
-                'd' => 1,
-                'm' => 10,
-                't' => 5,
-                _ => 0
-            },
-            
-            (c) => c switch {
-                'd' => 1,
-                'm' => 10,
-                '(' => 6,
-                _ => 0
-            },
-            
-            (c) => c switch {
-                'd' => 1,
-                'm' => 10,
-                ')' => 7,
-                _ => 0
-            },
-            
-            (c) => c switch {
-                'd' => 1,
-                'm' => 10,
-                _ => 0
-            },
-            
-            (c) => c switch {
-                'd' => 1,
-                'm' => 10,
-                ')' => 9,
-                _ => 0
-            },
-            
-            (c) => c switch {
-                'd' => 1,
-                'm' => 10,
-                _ => 0
-            },
-            
-            (c) => c switch {
-                 'd' => 1,
-                 'm' => 10,
-                 'u' => 11,
-                 _ => 0
-            },
-            
-            (c) => c switch {
-                 'd' => 1,
-                 'm' => 10,
-                 'l' => 12,
-                 _ => 0
-            },
-            
-            (c) => c switch {
-                'd' => 1,
-                'm' => 10,
-                '(' => 13,
-                _ => 0
-            },
-            
-            (c) => char.IsDigit(c) ? 14 : c switch {
-                'd' => 1,
-                'm' => 10,
-                _ => 0
-            },
-            
-            (c) => char.IsDigit(c) ? 14 : c switch {
-                'd' => 1,
-                'm' => 10,
-                ',' => 15,
-                _ => 0
-            },
-            
-            (c) => char.IsDigit(c) ? 16 : c switch {
-                'd' => 1,
-                'm' => 10,
-                _ => 0
-            },
-            
-            (c) => char.IsDigit(c) ? 16 : c switch {
-                'd' => 1,
-                'm' => 10,
-                ')' => 17,
-                _ => 0
-            },
-            
-            (c) => c switch {
-                'd' => 1,
-                'm' => 10,
-                _ => 0
-            },
-        };
         
         var currentState = 0;
         var enabled = true;
@@ -167,32 +39,203 @@ public class Day03
         var shortLen = text.Length - 1;
         for (var i = 0; i < text.Length; i++)
         {
-            currentState = fns[currentState](text[i]);
+          //  currentState = fns[currentState](text[i]);
+
+          var c = text[i];
+          currentState = currentState switch
+          {
+              0 => c switch
+              {
+                  'd' => 1,
+                  'm' => 10,
+                  _ => 0
+              },
+
+              //1
+              1 => c switch
+              {
+                  'd' => 1,
+                  'm' => 10,
+                  'o' => 2,
+                  _ => 0
+              },
+
+              //2
+              2 => c switch
+              {
+                  'd' => 1,
+                  'm' => 10,
+                  'n' => 3,
+                  '(' => 8,
+                  _ => 0
+              },
+
+              //3
+              3 => c switch
+              {
+                  'd' => 1,
+                  'm' => 10,
+                  '\'' => 4,
+                  _ => 0
+              },
+
+              4 => c switch
+              {
+                  'd' => 1,
+                  'm' => 10,
+                  't' => 5,
+                  _ => 0
+              },
+
+              5 => c switch
+              {
+                  'd' => 1,
+                  'm' => 10,
+                  '(' => 6,
+                  _ => 0
+              },
+
+              6 => c switch
+              {
+                  'd' => 1,
+                  'm' => 10,
+                  ')' => 7,
+                  _ => 0
+              },
+
+              7 => c switch
+              {
+                  'd' => 1,
+                  'm' => 10,
+                  _ => 0
+              },
+
+              8 => c switch
+              {
+                  'd' => 1,
+                  'm' => 10,
+                  ')' => 9,
+                  _ => 0
+              },
+
+              9 => c switch
+              {
+                  'd' => 1,
+                  'm' => 10,
+                  _ => 0
+              },
+
+              10 => c switch
+              {
+                  'd' => 1,
+                  'm' => 10,
+                  'u' => 11,
+                  _ => 0
+              },
+
+              11 => c switch
+              {
+                  'd' => 1,
+                  'm' => 10,
+                  'l' => 12,
+                  _ => 0
+              },
+
+              12 => c switch
+              {
+                  'd' => 1,
+                  'm' => 10,
+                  '(' => 13,
+                  _ => 0
+              },
+
+              13 => char.IsDigit(c)
+                  ? 14
+                  : c switch
+                  {
+                      'd' => 1,
+                      'm' => 10,
+                      _ => 0
+                  },
+
+              14 => char.IsDigit(c)
+                  ? 14
+                  : c switch
+                  {
+                      'd' => 1,
+                      'm' => 10,
+                      ',' => 15,
+                      _ => 0
+                  },
+
+              15 => char.IsDigit(c)
+                  ? 16
+                  : c switch
+                  {
+                      'd' => 1,
+                      'm' => 10,
+                      _ => 0
+                  },
+
+              16 => char.IsDigit(c)
+                  ? 16
+                  : c switch
+                  {
+                      'd' => 1,
+                      'm' => 10,
+                      ')' => 17,
+                      _ => 0
+                  },
+
+              17 => c switch
+              {
+                  'd' => 1,
+                  'm' => 10,
+                  _ => 0
+              },
+          };
+          
 
             switch (currentState)
             {
                 case 14:
                 {
-                    lhs = text[i] - '0';
-                    var next = i < shortLen ? text[i + 1] - '0' : -1;
-                    while (next is >= 0 and <= 9 )
+                    if (enabled)
                     {
-                        i++;
-                        lhs = (lhs * 10) + next;
-                        next = i < shortLen ? text[i + 1] - '0' : -1;
+                        lhs = text[i] - '0';
+                        var next = i < shortLen ? text[i + 1] - '0' : -1;
+                        while (next is >= 0 and <= 9)
+                        {
+                            i++;
+                            lhs = (lhs * 10) + next;
+                            next = i < shortLen ? text[i + 1] - '0' : -1;
+                        }
+                    }
+                    else
+                    {
+                        while (i < shortLen && char.IsDigit(text[i + 1]))
+                            i++;
                     }
 
                     break;
                 }
                 case 16:
                 {
-                    rhs = text[i] - '0';
-                    var next = i < shortLen ? text[i + 1] - '0' : -1;
-                    while (next is >= 0 and <= 9 )
+                    if (enabled)
                     {
-                        i++;
-                        rhs = (rhs * 10) + next;
-                        next = i < shortLen ? text[i + 1] - '0' : -1;
+                        rhs = text[i] - '0';
+                        var next = i < shortLen ? text[i + 1] - '0' : -1;
+                        while (next is >= 0 and <= 9)
+                        {
+                            i++;
+                            rhs = (rhs * 10) + next;
+                            next = i < shortLen ? text[i + 1] - '0' : -1;
+                        }
+                    }
+                    else
+                    {
+                        while (i < shortLen && char.IsDigit(text[i + 1]))
+                            i++;
                     }
 
                     break;
--- AdventOfCode2024/AdventOfCode2024.Solutions/Program.cs
+++ AdventOfCode2024/AdventOfCode2024.Solutions/Program.cs
@@ -1,3 +1,3 @@
 ﻿using AdventOfCode2024.Solutions;
 using BenchmarkDotNet.Running;
-BenchmarkRunner.Run<Day04BenchmarkTests>();
+BenchmarkRunner.Run<Day03BenchmarkTests>();
