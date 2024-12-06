10fc9bc4ec2f06ee2c869cddcfd1afeb0133871a
davidbetteridge <david.betteridge@zeromission.io>
2024-12-06 17:23:25 +0000

    Faster part 2

--- AdventOfCode2024/AdventOfCode2024.Solutions/Day06/Day06.cs
+++ AdventOfCode2024/AdventOfCode2024.Solutions/Day06/Day06.cs
@@ -2,7 +2,6 @@ namespace AdventOfCode2024.Solutions;
 
 public class Day06
 {
-    private record struct Location(int X, int Y);
     public int Part1(string filename)
     {
         var input = File.ReadAllBytes(filename).AsSpan();
@@ -78,68 +77,119 @@ public class Day06
         
         return visitedCount;
     }
+
     
     public int Part2(string filename)
     {
-        var lab = File.ReadAllLines(filename);
-        var guard = new Location(0,0);
-        var direction = 0;  // 0 means N,  1=E, 2=S and 3=W
-        
-        var obstructions = new HashSet<Location>();
-        var visited = new HashSet<Location>();
-
-        var width = lab[0].Length;
-        for (var rowNumber = 0; rowNumber < lab.Length; rowNumber++)
+        var input = File.ReadAllBytes(filename).AsSpan();
+        var map = new bool[input.Length];
+        var visited = new bool[input.Length];
+        var guard = -1;
+        var direction = 0; // 0 means N,  1=E, 2=S and 3=W
+        var width = -1;
+        var height = 0;
+        var i = 0;
+        while (i < input.Length)
         {
-            for (var columnNumber = 0; columnNumber < width; columnNumber++)
+            while (i < input.Length && input[i] != '\n')
             {
-                if (lab[rowNumber][columnNumber] == '#')
-                    obstructions.Add(new Location(columnNumber, rowNumber));
-                else if (lab[rowNumber][columnNumber] == '^')
-                    guard = new Location(columnNumber, rowNumber);
-            }
-        }
+                if (input[i] == '#')
+                    map[i-height] = true;
 
-        var initialLocation = guard;
+                if (input[i] == '^')
+                {
+                    guard = i - height;
+                    visited[guard]=true;
+                }
+
+                i++;
+            }
+
+            // End of the line
+            if (width == -1)
+                width = i;
+
+            height++;
         
-        visited.Add(guard);
+            i++; //eat the new line
+        }
+        
+        var initialLocation = guard;
         while (true)
         {
             // Try to walk forward
-            var next = NextLocation(guard, direction);
-            if (next.X < 0 || next.X >= width || next.Y < 0 || next.Y >= lab.Length) break;
+            var next = direction switch
+            {
+                0 => guard - width,
+                1 => guard + 1,
+                2 => guard + width,
+                3 => guard - 1,
+                _ => throw new Exception("currentDirection out of range")
+            };
             
-            if (obstructions.Contains(next))
+            // Would that be out of bounds
+            var ok = direction switch
+            {
+                0 => next >= 0,
+                1 => next % width != 0,  //Walked right, so can't be at x=0
+                2 => next < (width * height),
+                3 => next % width != (width-1),   // Walked left so can be width2-1
+                _ => throw new Exception("currentDirection out of range")
+            };
+            
+            if (!ok) break;
+            if (map[next])
                 direction = (direction + 1) % 4; // turn right
             else
             {
                 guard = next;
-                visited.Add(guard);
+                visited[guard]=true;
             }
         }
+        
         
         // Other than the initial location, we have to try adding an obstruction
         // in each location and seeing if it causes a loop.
         var positions = 0;
-        var route = new HashSet<(Location, int)>();
-        foreach (var possible in visited)
+        var route = new HashSet<(int, int)>();
+
+        for (var j = 0; j < input.Length; j++)
         {
-            if (possible != initialLocation)
+            if (visited[j] && j != initialLocation)
             {
-                obstructions.Add(possible);
+                map[j] = true;
                 guard = initialLocation;
                 direction = 0;
                 route.Clear();
                 while (true)
                 {
                     // Try to walk forward
-                    var next = NextLocation(guard, direction);
-                    if (obstructions.Contains(next))
+                    var next = direction switch
+                    {
+                        0 => guard - width,
+                        1 => guard + 1,
+                        2 => guard + width,
+                        3 => guard - 1,
+                        _ => throw new Exception("currentDirection out of range")
+                    };
+                    
+                    // Would that be out of bounds
+                    var ok = direction switch
+                    {
+                        0 => next >= 0,
+                        1 => next % width != 0,  //Walked right, so can't be at x=0
+                        2 => next < (width * height),
+                        3 => next % width != (width-1),   // Walked left so can be width2-1
+                        _ => throw new Exception("currentDirection out of range")
+                    };
+                    
+                    if (!ok) break;
+                    
+                    if (map[next])
                         direction = (direction + 1) % 4; // turn right
                     else
                     {
                         guard = next;
-                        if (guard.X < 0 || guard.X == width || guard.Y < 0 || guard.Y == lab.Length) break;
                         if (route.Contains((guard, direction)))
                         {
                             // Looping
@@ -149,23 +199,10 @@ public class Day06
                         route.Add((next, direction));
                     }
                 }
-                obstructions.Remove(possible);
+                map[j] = false;
             }
         }
-        
+       
         return positions;
-
-        Location NextLocation(Location currentLocation, int currentDirection)
-        {
-            return currentDirection switch
-            {
-                0 => currentLocation with { Y = currentLocation.Y - 1 },
-                1 => currentLocation with { X = currentLocation.X + 1 },
-                2 => currentLocation with { Y = currentLocation.Y + 1 },
-                3 => currentLocation with { X = currentLocation.X - 1 },
-                _ => throw new Exception("currentDirection out of range")
-            };
-        }
-   
     }
 }
