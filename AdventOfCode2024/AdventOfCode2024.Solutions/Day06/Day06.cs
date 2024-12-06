1af2ec856703fca9eb0734b5ee0b0a7b9757c95f
davidbetteridge <david.betteridge@zeromission.io>
2024-12-06 17:29:13 +0000

    Bit masks for win

--- AdventOfCode2024/AdventOfCode2024.Solutions/Day06/Day06.cs
+++ AdventOfCode2024/AdventOfCode2024.Solutions/Day06/Day06.cs
@@ -205,4 +205,133 @@ public class Day06
        
         return positions;
     }
+    
+    
+    
+    public int Part2b(string filename)
+    {
+        var input = File.ReadAllBytes(filename).AsSpan();
+        var map = new bool[input.Length];
+        var visited = new bool[input.Length];
+        var guard = -1;
+        var direction = 0; // 0 means N,  1=E, 2=S and 3=W
+        var width = -1;
+        var height = 0;
+        var i = 0;
+        while (i < input.Length)
+        {
+            while (i < input.Length && input[i] != '\n')
+            {
+                if (input[i] == '#')
+                    map[i-height] = true;
+
+                if (input[i] == '^')
+                {
+                    guard = i - height;
+                    visited[guard]=true;
+                }
+
+                i++;
+            }
+
+            // End of the line
+            if (width == -1)
+                width = i;
+
+            height++;
+        
+            i++; //eat the new line
+        }
+        
+        var initialLocation = guard;
+        while (true)
+        {
+            // Try to walk forward
+            var next = direction switch
+            {
+                0 => guard - width,
+                1 => guard + 1,
+                2 => guard + width,
+                3 => guard - 1,
+                _ => throw new Exception("currentDirection out of range")
+            };
+            
+            // Would that be out of bounds
+            var ok = direction switch
+            {
+                0 => next >= 0,
+                1 => next % width != 0,  //Walked right, so can't be at x=0
+                2 => next < (width * height),
+                3 => next % width != (width-1),   // Walked left so can be width2-1
+                _ => throw new Exception("currentDirection out of range")
+            };
+            
+            if (!ok) break;
+            if (map[next])
+                direction = (direction + 1) % 4; // turn right
+            else
+            {
+                guard = next;
+                visited[guard]=true;
+            }
+        }
+        
+        
+        // Other than the initial location, we have to try adding an obstruction
+        // in each location and seeing if it causes a loop.
+        var positions = 0;
+        var route = new HashSet<int>();
+
+        for (var j = 0; j < input.Length; j++)
+        {
+            if (visited[j] && j != initialLocation)
+            {
+                map[j] = true;
+                guard = initialLocation;
+                direction = 0;
+                route.Clear();
+                while (true)
+                {
+                    // Try to walk forward
+                    var next = direction switch
+                    {
+                        0 => guard - width,
+                        1 => guard + 1,
+                        2 => guard + width,
+                        3 => guard - 1,
+                        _ => throw new Exception("currentDirection out of range")
+                    };
+                    
+                    // Would that be out of bounds
+                    var ok = direction switch
+                    {
+                        0 => next >= 0,
+                        1 => next % width != 0,  //Walked right, so can't be at x=0
+                        2 => next < (width * height),
+                        3 => next % width != (width-1),   // Walked left so can be width2-1
+                        _ => throw new Exception("currentDirection out of range")
+                    };
+                    
+                    if (!ok) break;
+                    
+                    if (map[next])
+                        direction = (direction + 1) % 4; // turn right
+                    else
+                    {
+                        guard = next;
+                        if (route.Contains((next << 2) + direction))
+                        {
+                            // Looping
+                            positions++;
+                            break;
+                        }
+                        route.Add((guard << 2) + direction);
+                    }
+                }
+                map[j] = false;
+            }
+        }
+       
+        return positions;
+    }
 }
