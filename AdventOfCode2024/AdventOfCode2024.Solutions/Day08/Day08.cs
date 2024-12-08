a17b8ba0689b0cd803d71b5cd68132a7fbe371b1
davidbetteridge <david.betteridge@zeromission.io>
2024-12-08 10:19:37 +0000

    Day 8 - Part 2

--- AdventOfCode2024/AdventOfCode2024.Solutions/Day08/Day08.cs
+++ AdventOfCode2024/AdventOfCode2024.Solutions/Day08/Day08.cs
@@ -58,4 +58,67 @@ public class Day08
         
         return antinodes.Count;
     }
+    
+    
+    public long Part2(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        var height = lines.Length;
+        var width = lines[0].Length;
+        var antennas = new Dictionary<char, List<Location>>();
+
+        for (var rowNumber = 0; rowNumber < width; rowNumber++)
+        {
+            for (var columnNumber = 0; columnNumber < height; columnNumber++)
+            {
+                var cell = lines[rowNumber][columnNumber];
+                if (cell != '.')
+                {
+                    if (!antennas.ContainsKey(cell))
+                    {
+                        antennas[cell] = [];
+                    }
+                    antennas[cell].Add(new Location(columnNumber, rowNumber));
+                }
+            }
+        }
+
+        var antinodes = new HashSet<Location>();
+        
+        // Naive solution
+        foreach (var antenna in antennas)
+        {
+            var locations = antenna.Value;
+            for (var i = 0; i < locations.Count-1 ; i++)
+            {
+                for (var j = i+1; j < locations.Count; j++)
+                {
+                    var xDiff = locations[j].X - locations[i].X;
+                    var yDiff = locations[j].Y - locations[i].Y;
+                    
+                    var xTry = locations[i].X;
+                    var yTry = locations[i].Y;
+                    while (xTry >= 0 && yTry >= 0 && xTry < width && yTry < height)
+                    {
+                        antinodes.Add(new Location(xTry, yTry));
+                        xTry -= xDiff;
+                        yTry -= yDiff;
+                    }
+
+                    xTry = locations[j].X;
+                    yTry = locations[j].Y;
+                    while (xTry >= 0 && yTry >= 0 && xTry < width && yTry < height)
+                    {
+                        antinodes.Add(new Location(xTry, yTry));
+                        xTry += xDiff;
+                        yTry += yDiff;
+                    }
+
+                }
+            }
+        }
+        
+        
+        return antinodes.Count;
+    }
 }
--- AdventOfCode2024/AdventOfCode2024.Tests/Day08/Day08Tests.cs
+++ AdventOfCode2024/AdventOfCode2024.Tests/Day08/Day08Tests.cs
@@ -16,18 +16,18 @@ public class Day08Tests
         Assert.Equal(359, solver.Part1("Day08/input.txt")); 
     }
     
-    // [Fact]
-    // public void Test_Part2_Sample()
-    // {
-    //     var solver = new Solutions.Day08();
-    //     Assert.Equal(11387, solver.Part2("Day08/sample.txt"));
-    // }
-    //
-    // [Fact]
-    // public void Test_Part2()
-    // {
-    //     var solver = new Solutions.Day08();
-    //     Assert.Equal(145397611085341, solver.Part2("Day08/input.txt"));
-    // }
+    [Fact]
+    public void Test_Part2_Sample()
+    {
+        var solver = new Solutions.Day08();
+        Assert.Equal(9, solver.Part2("Day08/sample_2.txt"));
+    }
+    
+    [Fact]
+    public void Test_Part2()
+    {
+        var solver = new Solutions.Day08();
+        Assert.Equal(1293, solver.Part2("Day08/input.txt"));
+    }
 
  }
