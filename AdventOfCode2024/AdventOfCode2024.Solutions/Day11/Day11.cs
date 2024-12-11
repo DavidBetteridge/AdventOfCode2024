670c155c5185cc960e6493a864b4e9073ab6bf64
davidbetteridge <david.betteridge@zeromission.io>
2024-12-11 09:23:42 +0000

    Faster

--- AdventOfCode2024/AdventOfCode2024.Solutions/Day11/Day11.cs
+++ AdventOfCode2024/AdventOfCode2024.Solutions/Day11/Day11.cs
@@ -1,3 +1,5 @@
+using System.Collections.Concurrent;
+
 namespace AdventOfCode2024.Solutions;
 
 public class Day11
@@ -5,37 +7,48 @@ public class Day11
     public long Part1(string filename)
     {
         var stones = File.ReadAllText(filename).Split(' ').Select(long.Parse).ToArray();
-        var cache = new Dictionary<(long, int), long>();
+        var cache = new Dictionary<long, long[]>();
         return stones.Select(stone => Blink(25, stone, cache)).Sum();
     }
 
     public long Part2(string filename)
     {
         var stones = File.ReadAllText(filename).Split(' ').Select(long.Parse).ToArray();
-        var cache = new Dictionary<(long, int), long>();
+        var cache = new Dictionary<long, long[]>();
         return stones.Select(stone => Blink(75, stone, cache)).Sum();
     }
-    
-    private long Blink(int timesToBlink, long stone, Dictionary<(long,int),long> cache)
+
+    private long Blink(int timesToBlink, long stone, Dictionary<long, long[]> cache)
     {
-        if (cache.TryGetValue((stone, timesToBlink), out var x)) return x;
-        
         if (timesToBlink == 0) return 1;
+        if (cache.TryGetValue(stone, out var x) && x[timesToBlink] != 0) return x[timesToBlink];
 
         long newStoneCount;
         if (stone == 0)
             newStoneCount = Blink(timesToBlink - 1, 1, cache);
-        else if ((int)Math.Floor(Math.Log10(stone)) % 2 == 1)
+        else
         {
-            var e = (int)Math.Pow(10, ((int)Math.Floor(Math.Log10(stone)) + 1) / 2.0);
-            newStoneCount = Blink(timesToBlink - 1, stone / e, cache) +
-                            Blink(timesToBlink - 1, stone % e, cache);
+            var f = (int)Math.Log10(stone);
+            if (f % 2 == 1)
+            {
+                var e = (int)Math.Pow(10, (f + 1) / 2.0);
+                newStoneCount = Blink(timesToBlink - 1, stone / e, cache) +
+                                Blink(timesToBlink - 1, stone % e, cache);
+            }
+            else
+                newStoneCount = Blink(timesToBlink - 1, stone * 2024, cache);
+        }
+        
+        if (cache.TryGetValue(stone, out var y))
+        {
+            y[timesToBlink] = newStoneCount;
         }
         else
-            newStoneCount = Blink(timesToBlink -1 , stone * 2024, cache);
+        {
+            cache[stone] = new long[76];
+            cache[stone][timesToBlink] = newStoneCount;
+        }
 
-        cache[(stone, timesToBlink)] = newStoneCount;
-        
         return newStoneCount;
     }
 }
