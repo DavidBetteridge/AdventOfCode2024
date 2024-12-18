2871b35d2a081a8ba693b1b0f0b9f02b8ce426c1
davidbetteridge <david.betteridge@zeromission.io>
2024-12-18 08:51:01 +0000

    Day 18 - Part 2

--- AdventOfCode2024/AdventOfCode2024.Solutions/Day18/Day18.cs
+++ AdventOfCode2024/AdventOfCode2024.Solutions/Day18/Day18.cs
@@ -109,4 +109,122 @@ public class Day18
         
         return distances[^1];
     }
+    
+    
+    public string Part2(string filename, int size, int startFrom)
+    {
+        var memory = new bool[size * size];
+        var drops = File.ReadAllLines(filename);
+
+        for (var numberOfDrops = startFrom + 1; numberOfDrops < drops.Length; numberOfDrops++)
+        {
+            Array.Clear(memory);
+
+            foreach (var drop in drops[..numberOfDrops])
+            {
+                // x,y
+                var parts = drop.Split(',');
+                memory[int.Parse(parts[1]) * size + int.Parse(parts[0])] = true;
+            }
+
+            var distances = new Distance[size * size];
+            var queue = new PriorityQueue<int, Distance>();
+
+            var source = 0;
+            queue.Enqueue(source, 0);
+            distances[source] = 0;
+
+            for (var row = 0; row < size; row++)
+            {
+                for (var col = 0; col < size; col++)
+                {
+                    var v = ((row * size) + col);
+                    if (!memory[v])
+                    {
+                        if (v != source)
+                        {
+                            distances[v] = int.MaxValue;
+                        }
+                    }
+                    else
+                    {
+                        distances[v] = -1;
+                    }
+                }
+            }
+
+
+            while (queue.Count > 0)
+            {
+                var u = queue.Dequeue();
+                //       if (distances[u] == int.MaxValue) break;
+
+                var col = u % size;
+                var row = (u - col) / size;
+
+                //   if (col == size-1 && row == size-1) break;
+
+                //North
+                if (row > 0 && (!memory[(row - 1) * size + col]))
+                {
+                    var v = ((row - 1) * size) + col;
+                    var alt = distances[u] + 1;
+                    if (alt < distances[v])
+                    {
+                        distances[v] = alt;
+                        queue.Remove(v, out _, out _);
+                        queue.Enqueue(v, alt);
+                    }
+                }
+
+                //South
+                if ((row + 1) < size && (!memory[(row + 1) * size + col]))
+                {
+                    var v = ((row + 1) * size) + col;
+                    var alt = distances[u] + 1;
+                    if (alt < distances[v])
+                    {
+                        distances[v] = alt;
+                        queue.Remove(v, out _, out _);
+                        queue.Enqueue(v, alt);
+                    }
+                }
+
+                //West
+                if (col > 0 && (!memory[(row) * size + (col - 1)]))
+                {
+                    var v = ((row) * size) + (col - 1);
+                    var alt = distances[u] + 1;
+                    if (alt < distances[v])
+                    {
+                        distances[v] = alt;
+                        queue.Remove(v, out _, out _);
+                        queue.Enqueue(v, alt);
+                    }
+                }
+
+                //East
+                if ((col + 1) < size && (!memory[(row) * size + (col + 1)]))
+                {
+                    var v = ((row) * size) + (col + 1);
+                    var alt = distances[u] + 1;
+                    if (alt < distances[v])
+                    {
+                        distances[v] = alt;
+                        queue.Remove(v, out _, out _);
+                        queue.Enqueue(v, alt);
+                    }
+                }
+
+
+            }
+
+            if (distances[^1] == int.MaxValue)
+            {
+                return drops[numberOfDrops-1];
+            }
+        }
+
+        return "";
+    }
 }
--- AdventOfCode2024/AdventOfCode2024.Tests/Day18/Day18Tests.cs
+++ AdventOfCode2024/AdventOfCode2024.Tests/Day18/Day18Tests.cs
@@ -16,4 +16,17 @@ public class Day18Tests
         Assert.Equal(322, solver.Part1("Day18/input.txt", 71, 1024));
     }
     
+    [Fact]
+    public void Test_Part2_Sample()
+    {
+        var solver = new Solutions.Day18();
+        Assert.Equal("6,1", solver.Part2("Day18/sample.txt", 7, 12));
+    }
+    
+    [Fact]
+    public void Test_Part2()
+    {
+        var solver = new Solutions.Day18();
+        Assert.Equal("60,21", solver.Part2("Day18/input.txt", 71, 1024));
+    }
 }
