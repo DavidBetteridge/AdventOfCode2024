8e830ef235a0b40634eb1f0c1d8902d569bae026
davidbetteridge <david.betteridge@zeromission.io>
2024-12-19 08:11:19 +0000

    P2 - too slow

--- AdventOfCode2024/AdventOfCode2024.Solutions/Day19/Day19.cs
+++ AdventOfCode2024/AdventOfCode2024.Solutions/Day19/Day19.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace AdventOfCode2024.Solutions;
 
 public class Day19
@@ -30,6 +32,47 @@ public class Day19
             }
 
             return false;
+        }
+    }
+    
+    public long Part2(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        var towels = lines[0].Split(", ");
+        var towelsUsed = new int[towels.Length];
+
+        var total = 0L;
+        var possibleArrangements = new HashSet<string>();
+        foreach (var pattern in lines[2..])
+        {
+            possibleArrangements.Clear();
+            ValidCombinations(pattern);
+            total+=possibleArrangements.Count;
+        }
+
+        return total;
+        
+        void ValidCombinations(string patternToCheck)
+        {
+            if (patternToCheck == "")
+            {
+                // We have constructed the pattern from the towels listed in towelsUsed
+                var sb = new StringBuilder();
+                for (var towelNumber = 0; towelNumber < towels.Length; towelNumber++)
+                    sb.Append(towelsUsed[towelNumber] + ',');
+                possibleArrangements.Add(sb.ToString());
+            }
+            
+            for (var towelNumber = 0; towelNumber < towels.Length; towelNumber++)
+            {
+                if (patternToCheck.StartsWith(towels[towelNumber]))
+                {
+                    towelsUsed[towelNumber]++;
+                    ValidCombinations(patternToCheck[(towels[towelNumber].Length)..]);
+                    towelsUsed[towelNumber]--;
+                }  
+            }
+       
         }
     }
 }
--- AdventOfCode2024/AdventOfCode2024.Tests/Day19/Day19Tests.cs
+++ AdventOfCode2024/AdventOfCode2024.Tests/Day19/Day19Tests.cs
@@ -6,13 +6,27 @@ public class Day19Tests
     public void Test_Part1_Sample()
     {
         var solver = new Solutions.Day19();
-        Assert.Equal(333, solver.Part1("Day19/sample.txt"));
+        Assert.Equal(6, solver.Part1("Day19/sample.txt"));
     }
     
     [Fact]
     public void Test_Part1()
     {
         var solver = new Solutions.Day19();
-        Assert.Equal(322, solver.Part1("Day19/input.txt"));
+        Assert.Equal(333, solver.Part1("Day19/input.txt"));
+    }
+    
+    [Fact]
+    public void Test_Part2_Sample()
+    {
+        var solver = new Solutions.Day19();
+        Assert.Equal(16, solver.Part2("Day19/sample.txt"));
+    }
+    
+    [Fact]
+    public void Test_Part2()
+    {
+        var solver = new Solutions.Day19();
+        Assert.Equal(0, solver.Part2("Day19/input.txt"));
     }
 }
