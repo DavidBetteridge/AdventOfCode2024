b066a45bde998f5b37e422336fb31b6d34660f46
davidbetteridge <david.betteridge@zeromission.io>
2024-12-20 15:34:53 +0000

    Sample works

---
+++ AdventOfCode2024/AdventOfCode2024.Solutions/Day20/Day20_Part2.cs
@@ -0,0 +1,163 @@
+// ReSharper disable UseSymbolAlias
+namespace AdventOfCode2024.Solutions;
+using Distance = int;
+public class Day20_Part2
+{
+    public int Part2(string filename, int savesAtLeast, int cheatTime)
+    {
+        var map = File.ReadAllLines(filename);
+        var height = map.Length;
+        var width = map[0].Length;
+        
+        var walls = new bool[height * width];
+
+        var start = (0, 0);
+        var end = (0, 0);
+        for (var rowNumber = 0; rowNumber < height; rowNumber++)
+        {
+            for (var columnNumber = 0; columnNumber < width; columnNumber++)
+            {
+                if (map[rowNumber][columnNumber] == '#')
+                    walls[rowNumber * width + columnNumber] = true;
+
+                if (map[rowNumber][columnNumber] == 'S')
+                    start = (columnNumber, rowNumber);
+                
+                if (map[rowNumber][columnNumber] == 'E')
+                    end = (columnNumber, rowNumber);
+            }
+        }
+        
+       
+        var forward = CostMap(height, width, start, walls);
+        var backwards = CostMap(height, width, end, walls).Cost;
+        
+        var target = end.Item2 * width + end.Item1;
+        var worstCaseCost = forward.Cost[target];
+        
+        var location = target;
+        var source = start.Item2 * width + start.Item1;
+        var goodCheats = 0;
+        while (true)
+        {
+            var columnNumber = location % width;
+            var rowNumber = location / width;
+
+            // Is there a non-walled cell within 20 spaces in any direction
+            // which cost from the end + distance from us + plus our distance
+            // is 100 less than the worst case cost.
+            var done = false;
+            for (var yOffset = -cheatTime; yOffset <= cheatTime; yOffset++)
+            {
+                for (var xOffset = -cheatTime; xOffset <= cheatTime; xOffset++)
+                {
+                    var newX = columnNumber + xOffset;
+                    var newY = rowNumber + yOffset;
+                    if (newX > 0 && newX < (width - 1) && newY > 0 && newY < (height - 1))
+                    {
+                        var newLoc = newY * width + newX;
+                        if (!walls[newLoc])
+                        {
+                            var offset = Math.Abs(yOffset) + Math.Abs(xOffset);
+                            var cost = backwards[newLoc] + forward.Cost[location] + offset;
+                            if ((worstCaseCost - cost) >= savesAtLeast)
+                            {
+                                goodCheats++;
+                                // done = true;
+                                // break;
+                            }
+                        }
+                    }
+                }
+
+             //   if (done) break;
+            }
+            
+            if (location == source) break;
+            location = forward.Path[location];
+        }
+
+        return goodCheats;
+
+    }
+
+    private static (int[] Path, Distance[] Cost) CostMap(int height, int width, (int, int) start, bool[] walls)
+    {
+        var distances = new Distance[height * width];
+        var prev = new int[height * width];
+        var queue = new PriorityQueue<int, Distance>();
+        
+        var source = start.Item2 * width + start.Item1;
+        queue.Enqueue(source,0);
+
+        Array.Fill(distances, Distance.MaxValue);
+        distances[source] = 0;
+
+        while (queue.Count > 0)
+        {
+            var u = queue.Dequeue();
+
+            var col = u % width;
+            var row = u / width;
+
+            //North
+            var v = u - width;
+            if (row > 0 && (!walls[v]))
+            {
+                var alt = distances[u] + 1;
+                if (alt < distances[v])
+                {
+                    distances[v] = alt;
+                    prev[v] = u;
+                    queue.Remove(v, out _, out _);
+                    queue.Enqueue(v, alt);
+                }
+            }
+            
+            //South
+            v = u + width;
+            if ((row + 1) < height && (!walls[v]))
+            {
+                var alt = distances[u] + 1;
+                if (alt < distances[v])
+                {
+                    distances[v] = alt;
+                    prev[v] = u;
+                    queue.Remove(v, out _, out _);
+                    queue.Enqueue(v, alt);
+                }
+            }
+
+            //West
+            v = u - 1;
+            if (col > 0 && (!walls[v]))
+            {
+                var alt = distances[u] + 1;
+                if (alt < distances[v])
+                {
+                    distances[v] = alt;
+                    prev[v] = u;
+                    queue.Remove(v, out _, out _);
+                    queue.Enqueue(v, alt);
+                }
+            }
+
+            //East
+            v = u + 1;
+            if ((col + 1) < width && (!walls[v]))
+            {
+                var alt = distances[u] + 1;
+                if (alt < distances[v])
+                {
+                    distances[v] = alt;
+                    prev[v] = u;
+                    queue.Remove(v, out _, out _);
+                    queue.Enqueue(v, alt);
+                }
+            }
+
+        }
+
+        return (prev, distances);
+    }
+}
--- AdventOfCode2024/AdventOfCode2024.Tests/Day20/Day20Tests.cs
+++ AdventOfCode2024/AdventOfCode2024.Tests/Day20/Day20Tests.cs
@@ -5,14 +5,28 @@ public class Day20Tests
     [Fact]
     public void Test_Part1_Sample()
     {
-        var solver = new Solutions.Day20();
+        var solver = new Solutions.Day20_Part1();
         Assert.Equal(44, solver.Part1("Day20/sample.txt", 1));
     }
     
     [Fact]
     public void Test_Part1()
     {
-        var solver = new Solutions.Day20();
-        Assert.Equal(1507, solver.Part1("Day20/input.txt", 100));  // 944 too low
+        var solver = new Solutions.Day20_Part1();
+        Assert.Equal(1507, solver.Part1("Day20/input.txt", 100));
+    }
+    
+    [Fact]
+    public void Test_Part2_Sample()
+    {
+        var solver = new Solutions.Day20_Part2();
+        Assert.Equal(41, solver.Part2("Day20/sample.txt", 70, 20));
+    }
+    
+    [Fact]
+    public void Test_Part2()
+    {
+        var solver = new Solutions.Day20_Part2();
+        Assert.Equal(1507, solver.Part2("Day20/input.txt", 100, 20));  // 2463365 too high
     }
 }
