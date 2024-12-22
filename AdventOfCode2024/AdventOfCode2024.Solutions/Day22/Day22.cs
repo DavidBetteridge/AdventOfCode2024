d2e198cc70e6e3142366da7d8ed344edeccfca58
davidbetteridge <david.betteridge@zeromission.io>
2024-12-22 10:54:56 +0000

    Fast parse

--- AdventOfCode2024/AdventOfCode2024.Solutions/Day22/Day22.cs
+++ AdventOfCode2024/AdventOfCode2024.Solutions/Day22/Day22.cs
@@ -6,13 +6,25 @@ public class Day22
 {
     public long Part1(string filename)
     {
-        var nums = File.ReadAllLines(filename).Select(long.Parse).ToArray();
+        //var nums = File.ReadAllLines(filename).Select(long.Parse).ToArray();
 
+        var input = File.ReadAllBytes(filename);
+        var i = 0;
+        var nums = new List<long>();
+        while (i < input.Length)
+        {
+            var num = input[i++] - '0';
+            while (i < input.Length && input[i] != '\n')
+                num = num * 10 + input[i++] - '0';
+            i++;
+            nums.Add(num);
+        }
+        
         var totals = new ConcurrentBag<long>();
         Parallel.ForEach(nums, num =>
         {
             var secretNumber = num;
-            for (var i = 0; i < 2000; i++)
+            for (var iteration = 0; iteration < 2000; iteration++)
             {
                 var nextNumber =secretNumber << 6;
                 secretNumber = nextNumber ^ secretNumber;
