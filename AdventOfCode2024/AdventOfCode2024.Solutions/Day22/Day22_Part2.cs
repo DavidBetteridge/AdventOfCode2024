205fc36d41102949dc66a4996eb84163cd58f360
davidbetteridge <david.betteridge@zeromission.io>
2024-12-22 11:18:07 +0000

    Faster

--- AdventOfCode2024/AdventOfCode2024.Solutions/Day22/Day22_Part2.cs
+++ AdventOfCode2024/AdventOfCode2024.Solutions/Day22/Day22_Part2.cs
@@ -4,34 +4,42 @@ public class Day22_Part2
 {
     public long Part2(string filename, int numberOfIterations)
     {
-        // 16,777,216 needs 24 bits, plus 1 bits for a sign
-        // Need 4 offsets,  so 100bits
+        var input = File.ReadAllBytes(filename);
+        var i = 0;
+        var nums = new List<int>();
+        while (i < input.Length)
+        {
+            var num = input[i++] - '0';
+            while (i < input.Length && input[i] != '\n')
+                num = num * 10 + input[i++] - '0';
+            i++;
+            nums.Add(num);
+        }
         
-        var nums = File.ReadAllLines(filename).Select(long.Parse).ToArray();
-        var totalByOffset = new Dictionary<Int128, int>();
+        var totalByOffset = new Dictionary<Int128, uint>();
         var seenOffsets = new HashSet<Int128>();
         var mask = (Int128)(Math.Pow(2, 100)) - 1;
         foreach (var num in nums)
         {
-            var secretNumber = num;
+            var secretNumber = (uint)num;
             var previousPrice = secretNumber%10;
             System.Int128 offsets = 0;
             seenOffsets.Clear();
-            for (var i = 0; i < numberOfIterations; i++)
+            for (var iteration = 0; iteration < numberOfIterations; iteration++)
             {
-                var nextNumber =secretNumber* 64;
+                var nextNumber =secretNumber << 6;
                 secretNumber = nextNumber ^ secretNumber;
                 secretNumber = secretNumber % 16777216;
 
-                nextNumber = (long)(secretNumber / 32);
+                nextNumber = (uint)(secretNumber >> 5);
                 secretNumber = nextNumber ^ secretNumber;
                 secretNumber = secretNumber % 16777216;
 
-                nextNumber =secretNumber* 2048;
+                nextNumber =secretNumber << 11;
                 secretNumber = nextNumber ^ secretNumber;
                 secretNumber = secretNumber % 16777216;
 
-                var price = (int)secretNumber % 10;
+                var price = secretNumber % 10;
                 var offset = price - previousPrice;
                 offsets = (offsets << 25) + (offset + 16777216);
                 offsets &= mask;
