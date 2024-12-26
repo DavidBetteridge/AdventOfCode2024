54674085c9d6419aa414140d22d8edf93c08c35d
davidbetteridge <david.betteridge@zeromission.io>
2024-12-26 10:22:43 +0000

    Faster

--- AdventOfCode2024/AdventOfCode2024.Solutions/Day25/Day25.cs
+++ AdventOfCode2024/AdventOfCode2024.Solutions/Day25/Day25.cs
@@ -8,8 +8,8 @@ public class Day25
     {
         var input = File.ReadAllBytes(filename);
 
-        var keys = new List<int[]>();
-        var locks = new List<int[]>();
+        var keys = new List<uint>();
+        var locks = new List<uint>();
 
         var i = 0;
         while (i < input.Length)
@@ -19,21 +19,19 @@ public class Day25
 
             if (isKey)
             {
-                var key = new int[6] { 5, 5, 5, 5, 5, 25 };
+                uint key = 0;
                 for (var r = 0; r < 5; r++)
                 {
                     for (var c = 0; c < 5; c++)
                     {
-                        if (input[i] == '.')
+                        key <<= 1;
+                        if (input[i] == '#')
                         {
-                            key[c]--;
+                            key ++;
                         }
 
                         i++;
                     }
-
-                    key[5] = key[0] + key[1] + key[2] + key[3] + key[4];
-
                     i++; // New line
                 }
 
@@ -41,20 +39,19 @@ public class Day25
             }
             else
             {
-                var loc = new int[6] { 0, 0, 0, 0, 0, 0 };
+                uint loc = 0;
                 for (var r = 0; r < 5; r++)
                 {
                     for (var c = 0; c < 5; c++)
                     {
-                        if (input[i] == '.')
+                        loc <<= 1;
+                        if (input[i] == '#')
                         {
-                            loc[c]++;
+                            loc++;
                         }
 
                         i++;
                     }
-
-                    loc[5] = loc[0] + loc[1] + loc[2] + loc[3] + loc[4];
                     i++; // New line
                 }
 
@@ -71,14 +68,7 @@ public class Day25
         {
             foreach (var loc in locks)
             {
-                var ok = loc[5] >= key[5];
-                ok &= loc[0] >= key[0];
-                ok &= loc[1] >= key[1];
-                ok &= loc[2] >= key[2];
-                ok &= loc[3] >= key[3];
-                ok &= loc[4] >= key[4];
-
-                if (ok)
+                if ((loc & key) == 0)
                     total.Add(1);
             }
         });
