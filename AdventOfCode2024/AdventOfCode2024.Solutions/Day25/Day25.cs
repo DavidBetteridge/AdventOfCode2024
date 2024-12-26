8c9b867f2421de70b41127c8a9dcc4a6431d3bb6
davidbetteridge <david.betteridge@zeromission.io>
2024-12-26 00:17:10 +0000

    Better parser

--- AdventOfCode2024/AdventOfCode2024.Solutions/Day25/Day25.cs
+++ AdventOfCode2024/AdventOfCode2024.Solutions/Day25/Day25.cs
@@ -4,43 +4,53 @@ public class Day25
 {
     public int Part1(string filename)
     {
-        var input = File.ReadAllText(filename).Split("\n\n");
+        var input = File.ReadAllBytes(filename);
 
         var keys = new List<int[]>();
         var locks = new List<int[]>();
-        foreach (var block in input)
-        {
-            var lines = block.Split('\n');
-            var isKey = lines[0] == ".....";
 
+        var i = 0;
+        while (i < input.Length)
+        {
+            var isKey = input[i] == '.';
+            i += 6;
+            
             if (isKey)
             {
                 var key = new int[5] { 5, 5, 5, 5, 5 };
-                for (var r = 1; r < 6; r++)
+                for (var r = 0; r < 5; r++)
                 {
                     for (var c = 0; c < 5; c++)
                     {
-                        if (lines[r][c] == '.')
+                        if (input[i] == '.')
                             key[c]--;
+                        i++;
                     }
+
+                    i++; // New line
                 }
+
                 keys.Add(key);
             }
             else
             {
                 var loc = new int[5] { 0,0,0,0,0 };
-                for (var r = 1; r < 6; r++)
+                for (var r = 0; r < 5; r++)
                 {
                     for (var c = 0; c < 5; c++)
                     {
-                        if (lines[r][c] == '.')
+                        if (input[i] == '.')
                             loc[c]++;
+                        i++;
                     }
+                    i++; // New line
                 }
-                locks.Add(loc);    
+                locks.Add(loc);
             }
-            
+
+            i += 7;  // Trailer and blank line
         }
+        
 
         var total = 0;
         foreach (var key in keys)
@@ -48,9 +58,9 @@ public class Day25
             foreach (var loc in locks)
             {
                 var ok = true;
-                for (var i = 0; i < 5; i++)
+                for (var j = 0; j < 5; j++)
                 {
-                    if (key[i] > loc[i])
+                    if (key[j] > loc[j])
                     {
                         ok = false;
                         break;
